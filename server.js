2b4e25be60e02e7649cd4b579cbdaebf5f97d829
Lennert Vanden Herrewegene <lvhroen@hotmail.comm>
2018-11-19 11:56:35 +0100

    test

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require("path");
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/app', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/app.js'));
+});
+
+app.get('/settings', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/settings.json'));
+});
+
+
+let init = () => {
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+};
+
+init();
