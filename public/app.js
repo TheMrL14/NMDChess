7bc3ef4659f02710cb0ecdf61e049bd0b4216ace
Lennert Vanden Herrewegene <lvhroen@hotmail.comm>
2018-11-19 16:52:04 +0100

    chess

--- public/app.js
+++ public/app.js
@@ -4,77 +4,79 @@ var sets = {};
 
 var settingrequest = new XMLHttpRequest();
 settingrequest.open('GET', './settings', true);
-settingrequest.onload = function () {
-    //load settings
-    var settings = JSON.parse(this.response);
-    sets = settings;
-    //start viz
-    var myp5 = new p5(s, sets.vizID);
+settingrequest.onload = function() {
+  //load settings
+  var settings = JSON.parse(this.response);
+  sets = settings;
+  //start viz
+  var myp5 = new p5(s, sets.vizID);
 }
 settingrequest.send();
 init();
 
 function init() {
 
-    var myp5 = new p5(s, sets.chessId);
-    console.log("done");
+  var myp5 = new p5(s, sets.chessId);
+  console.log("done");
 }
 
 
-var s = function (sketch) {
-    const settings = sets.sketch;
-    var rectWidth = 50;
-    var chessboard = []
+var s = function(sketch) {
+  const settings = sets.sketch;
+  var rectWidth = 50;
+  var chessboard = []
 
-    sketch.setup = function () {
-        sketch.createCanvas(640, 480);
-        sketch.rectMode(RADIUS);
-        for (var y = 0; y < 7; y++) {
-            for (var x = 0; x < 7; x++) {
-                var col = 0;
-                if ((x + y) % 2 == 0) {
-                    col = 0;
-                } else {
-                    col=255;
-                }
-                var chessrect = new ChessRect(sketch.createVector(x, y), rectWidth, col);
-    
-                chessboard.push(chessrect)
-            }
+  sketch.setup = function() {
+    sketch.createCanvas(640, 480);
+    sketch.rectMode(RADIUS);
+    for (var y = 1; y <= 8; y++) {
+      for (var x = 1; x <= 8; x++) {
+        var col = 0;
+        if ((x + y + 1) % 2 == 0) {
+          col = 0;
+        } else {
+          col = 255;
         }
+        var chessrect = new ChessRect(sketch.createVector(x, y), rectWidth, col);
+        chessboard.push(chessrect)
+      }
     }
+    console.log(chessboard)
+  }
 
 
-    sketch.draw = function () {
-        for (var i = 0; i < chessboard.length; i++) {
-            chessboard[i].show();
-        }
+  sketch.draw = function() {
+    for (var i = 0; i < chessboard.length; i++) {
+      chessboard[i].show();
+
     }
+  }
 
-    sketch.mousePressed = function() {
-        for (var i = 0; i < chessboard.length; i++) {
-            chessboard[i].clicked(sketch.mouseX, sketch.mouseY);
-            
-        }
+  sketch.mousePressed = function() {
+    for (var i = 0; i < chessboard.length; i++) {
+      chessboard[i].clicked(sketch.mouseX, sketch.mouseY);
+
     }
+  }
 
-    function ChessRect(pos, rectWidth, color) {
-        this.pos = pos;
-        this.rectWidth = rectWidth;
+  function ChessRect(pos, rectWidth, color) {
+    this.pos = pos;
+    this.rectWidth = rectWidth;
+    this.color = color;
+    this.show = function() {
+      sketch.fill(this.color);
+      sketch.rect(pos.x * rectWidth, pos.y * rectWidth, rectWidth / 2, rectWidth / 2);
+    }
+    this.clicked = function(px, py) {
+      var distance = sketch.dist(px, py, this.pos.x * rectWidth, this.pos.y * rectWidth);
+
+      if (distance < rectWidth / 2) {
+        this.color = 100;
+        sketch.fill(color);
+      } else {
         this.color = color;
-        this.show = function () {
-            sketch.fill(color);
-            sketch.rect(pos.x * rectWidth, pos.y * rectWidth, rectWidth, rectWidth);
-        }
-        this.clicked = function (px, py) {
-            var distance = sketch.dist(px, py, this.pos.x, this.pos.y);
-            console.log(this.pos);
-            if (distance < 10) {
-                this.color = 100;
-            }
-            else {
-                this.color = color;
-            }
-        }
+        sketch.fill(color);
+      }
     }
+  }
 }
