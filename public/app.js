3d4b5aac69c8342373338337bac0a30281dbad82
YOUR_USERNAME <titusdemaupeou@gmail.com>
2018-11-19 15:14:01 +0100

    modified

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
-var rectWidth = 50;
+var rectWidth = 60;
 var chessboard = []
 
 function setup() {
     createCanvas(640, 480);
     for (var y = 0; y < 7; y++) {
-        for (var x = 0; x < 7; x++) {
-            
+        for (var x = 0; x < 7; x++) { 
             var chessrect = new ChessRect(createVector(x,y),rectWidth) ;
             chessboard.push(chessrect)
         }
@@ -16,7 +15,6 @@ function setup() {
 function draw() {
     for(var i = 0; i < chessboard.length; i++) {
         chessboard[i].show();
-
     }
 }
 
@@ -32,5 +30,4 @@ function ChessRect(pos, rectWidth) {
         }
         rect(pos.x * rectWidth, pos.y * rectWidth, rectWidth, rectWidth); 
     }
-
 }
